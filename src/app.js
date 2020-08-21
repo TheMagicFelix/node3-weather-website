f3bbdd10c1e9367c332b56ece9c1016d6358493e
TheMagicFelix <riccardo.felisi01@gmail.com>
2020-08-21 14:43:13 +0200

    Init commit

---
+++ src/app.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+// define paths for express config
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// setup handlebars engine and views location
+
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
+
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Riccardo Felisi',
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About me',
+		name: 'Riccardo Felisi',
+		message: 'This is ',
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Help me!!!',
+		name: 'Riccardo Felisi',
+		text: 'This is a span',
+	});
+});
+
+app.get('/weather', (req, res) => {
+	if (!req.query.address) {
+		return res.send({
+			error: 'Please provide a valid address.',
+		});
+	}
+	geocode(
+		req.query.address,
+		(error, { latitude, longitude, location } = {}) => {
+			if (error) {
+				return res.send({ error });
+			}
+			forecast(latitude, longitude, (error, forecastData) => {
+				if (error) {
+					return res.send({ error });
+				}
+				res.send({
+					forecast: forecastData,
+					location,
+					address: req.query.address,
+				});
+			});
+		}
+	);
+});
+
+app.get('/products', (req, res) => {
+	if (!req.query.search) {
+		return res.send({
+			error: 'No such search term.',
+		});
+	}
+	res.send({
+		products: [],
+	});
+});
+
+app.get('/help/*', (req, res) => {
+	res.render('404', {
+		title: '404',
+		name: ' Felisi Riccardo',
+		errorMessage: 'Help article not found',
+	});
+});
+
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: '404',
+		name: 'Felisi Riccardo',
+		errorMessage: 'Page not found',
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000.');
+});
